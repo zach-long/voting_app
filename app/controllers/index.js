810caff2c7990d96e4d3e0abe2a2e48ff206c9f9
Zach <trombonezach@gmail.com>
2017-01-12 16:59:57 -0500

    Added poll routing logic

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,11 +12,42 @@ router.get('/', (req, res) => {
 router.get('/poll', (req, res) => {
   if (req.user) {
     console.log("User is logged in and can create a poll")
+
+    var pollID
+    do {
+      pollID = generateRandomNumber()
+    } while (pollIDExists(pollID))
+
+    res.redirect('/poll/' + pollID)
   } else {
     console.log("User cannot create a poll")
+    res.redirect('/')
   }
 
-  res.redirect('/')
+
 })
+
+router.get('/poll/:pollID*', (req, res) => {
+  if (req.user) {
+    // check if poll exists already
+      // display from DB
+    res.render('polls')
+  } else {
+    console.log("You cannot be here")
+    res.redirect('/')
+  }
+})
+
+function generateRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 999999).toString()
+  return randomNumber
+}
+
+function pollIDExists(num) {
+  // check db for existing poll ID
+    // if exists then return true
+  // else
+    // return false
+}
 
 module.exports = router
