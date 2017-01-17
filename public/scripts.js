c0e4b1ff3b9c0d39e5a5beee3964250778d4bc8b
Zach <trombonezach@gmail.com>
2017-01-17 10:37:38 -0500

    Poll creator can add more options

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,22 @@
 'use strict'
 
-// adds another input field to poll creation
-function addInput() {
-  var pollNameWrapper = document.getElementById('poll-name-wrapper');
-  console.log("Add a field under " + pollNameWrapper)
-}
+(function() {
+
+  // define variable placeholders
+  var addOptionButton;
+  // define globals
+  var optionsField = document.getElementsByClassName('options')[0];
+
+  // set event listener to add button functionality
+  addOptionButton = document.getElementById('add-option');
+  addOptionButton.addEventListener('click', addInput);
+
+  /* adds another input field to poll creation
+     - called from 'addOptionButton' event listener */
+  function addInput() {
+    let div = document.createElement('div');
+    div.innerHTML = '<label>Option</label><input type="text" name="option">';
+    optionsField.insertAdjacentElement('beforeend', div);
+  };
+
+}());
